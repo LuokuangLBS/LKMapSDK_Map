eb843cfb61a5ae9f82a0a610b3d12450c1dc6564
haofp@luokung.com <haofp@luokung.com>
2020-02-15 16:16:47 +0800

    Initial commit

---
+++ Example/LKMapSDK_Map/LKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LKAppDelegate.h
+//  LKMapSDK_Map
+//
+//  Created by LuokuangLBS on 02/15/2020.
+//  Copyright (c) 2020 LuokuangLBS. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LKMapSDK_Map/LKViewController.h
@@ -0,0 +1,13 @@
+//
+//  LKViewController.h
+//  LKMapSDK_Map
+//
+//  Created by LuokuangLBS on 02/15/2020.
+//  Copyright (c) 2020 LuokuangLBS. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LKViewController : UIViewController
+
+@end
